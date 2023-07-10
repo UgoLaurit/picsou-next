85d6b73bffa7e4b8ed5674dbd20409e28841ee6c
Ugo Laurit <u.laurit@gmail.com>
2023-07-10 23:45:18 +0200

    refactor: remove unused classes from NavBar

--- components/navigation/NavBar.tsx
+++ components/navigation/NavBar.tsx
@@ -31,76 +31,47 @@ export const NavBar = () => {
       <nav className='h-screen w-20 bg-[#3B4252] text-[#FFFFFF] relative border-r-2 border-[#2E3440]'>
         <ul className='flex flex-col items-center h-full gap-12 px-3 py-4'>
           <li>
-            <Link href='/' className='flex flex-row items-center gap-2'>
+            <Link href='/'>
               <img src='/icon.png' className='w-12 h-12' />
-              {/* <span>Compte courant</span> */}
             </Link>
           </li>
 
           <li className='flex flex-col gap-2 p-1.5 bg-[#81A1C1] rounded-xl'>
-            {/* <Link href='#' className='flex flex-row items-center gap-2'>
-              <Image src='/user.svg' alt={''} width={44} height={44} />
-              <span>Ugo</span>
-            </Link> */}
             <ul className='flex flex-col gap-2 opacity-100'>
               <li className={`${pathname === '/ugo/wallets' ? 'active' : ''}`}>
-                <Link
-                  href='/ugo/wallets'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/ugo/wallets'>
                   <WalletIcon color={'#415161'} />
-                  {/* <span>Compte courant</span> */}
                 </Link>
               </li>
               <li className={`${pathname === '/ugo/savings' ? 'active' : ''}`}>
-                <Link
-                  href='/ugo/savings'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/ugo/savings'>
                   <SavingsIcon color={'#415161'} />
-                  {/* <span>Épargne</span> */}
                 </Link>
               </li>
               <li
                 className={`${pathname === '/ugo/investments' ? 'active' : ''}`}
               >
-                <Link
-                  href='/ugo/investments'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/ugo/investments'>
                   <InvestIcon color={'#415161'} />
-                  {/* <span>Investissements</span> */}
                 </Link>
               </li>
               <li className={`${pathname === '/ugo/assets' ? 'active' : ''}`}>
-                <Link
-                  href='/ugo/assets'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/ugo/assets'>
                   <AssetIcon color={'#415161'} />
-                  {/* <span>Patrimoine</span> */}
                 </Link>
               </li>
             </ul>
           </li>
 
           <li className='flex flex-col gap-2 p-1.5 bg-[#8FBCBB] rounded-xl'>
-            {/* <Link href='#' className='flex flex-row items-center gap-2'>
-              <Image src='/users.svg' alt={''} width={44} height={44} />
-              <span>Compte joint</span>
-            </Link> */}
             <ul className='flex flex-col gap-2 opacity-100'>
               <li
                 className={`${
                   pathname === '/joint-account/wallets' ? 'active' : ''
                 }`}
               >
-                <Link
-                  href='/joint-account/wallets'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/joint-account/wallets'>
                   <WalletIcon color={'#485e5e'} />
-                  {/* <span>Compte courant</span> */}
                 </Link>
               </li>
               <li
@@ -108,55 +79,35 @@ export const NavBar = () => {
                   pathname === '/joint-account/savings' ? 'active' : ''
                 }`}
               >
-                <Link
-                  href='/joint-account/savings'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/joint-account/savings'>
                   <SavingsIcon color={'#485e5e'} />
-                  {/* <span>Épargne</span> */}
                 </Link>
               </li>
             </ul>
           </li>
 
           <li className='flex flex-col gap-2 p-1.5 bg-[#EBCB8B] rounded-xl'>
-            {/* <Link href='#' className='flex flex-row items-center gap-2'>
-              <Image src='/user.svg' alt={''} width={44} height={44} />
-              <span>Rima</span>
-            </Link> */}
             <ul className='flex flex-col gap-2 opacity-100'>
               <li className={`${pathname === '/rima/wallets' ? 'active' : ''}`}>
-                <Link
-                  href='/rima/wallets'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/rima/wallets'>
                   <WalletIcon color={'#766646'} />
-                  {/* <span>Compte courant</span> */}
                 </Link>
               </li>
               <li className={`${pathname === '/rima/savings' ? 'active' : ''}`}>
-                <Link
-                  href='/rima/savings'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/rima/savings'>
                   <SavingsIcon color={'#766646'} />
-                  {/* <span>Épargne</span> */}
                 </Link>
               </li>
               <li className={`${pathname === '/rima/assets' ? 'active' : ''}`}>
-                <Link
-                  href='/rima/assets'
-                  className='flex flex-row items-center gap-2'
-                >
+                <Link href='/rima/assets'>
                   <AssetIcon color={'#766646'} />
-                  {/* <span>Patrimoine</span> */}
                 </Link>
               </li>
             </ul>
           </li>
 
           <li className={`mt-auto ${pathname === '/upload' ? 'active' : ''}`}>
-            <Link href='/upload' className='flex flex-row items-center gap-2'>
+            <Link href='/upload'>
               <UploadIcon color={'#FFFFFF'} />
             </Link>
           </li>
